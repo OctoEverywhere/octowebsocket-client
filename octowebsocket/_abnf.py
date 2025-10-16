41f4015cb85367a82a7540988265510033c9b852
Quinn Damerell <quinnd@outlook.com>
2025-10-16 13:51:43 -0700

    test- small change.

--- octowebsocket/_abnf.py
+++ octowebsocket/_abnf.py
@@ -203,7 +203,7 @@ class ABNF:
 
             data_bytes = (
                 self.data[:2]
-                if isinstance(self.data, bytes)
+                if isinstance(self.data, (bytes, bytearray)) # OctoChange - handle bytearray
                 else self.data[:2].encode("utf-8")
             )
             code = struct.unpack("!H", data_bytes)[0]
